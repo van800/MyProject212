e7199357db2fa63a6f40bae4b8039a71f43cc905
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-11-30 16:19:44 +0100

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using ClassLibrary26;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Class1.Test();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
